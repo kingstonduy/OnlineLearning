7859a8998143948979ac8ac58cc0f3cb9086554d
ADMIN <kingstonduy.duong@gmail.com>
2023-04-25 01:06:53 +0700

    react

---
+++ Udemy/Master Spring and Spring boot/projects/learn-react/todo-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import TodoApp from './components/todo/TodoApp';
+
+
+
+function App() {
+  return (
+    <div className='App'>
+      <TodoApp />
+    </div>
+  );
+}
+export default App;
+
+
+
+
+
+
